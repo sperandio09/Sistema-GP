f2535543ba94c29457490baebdb1e34a19342925
Luma Sperandio <luma.sperandio@aluno.senai.br>
2026-09-02 07:53:27 -0400

    Organização pastas frontend

---
+++ Frontend/assets/api.js
@@ -0,0 +1,17 @@
+const API_URL = "https://sistema-gp-production.up.railway.app";
+
+async function cadastrarEscuderia(nome, turma, id_mentor){
+    const resposta = await fetch('${API_URL}/escuderias', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ nome, turma, id_mentor })
+    });
+    return resposta.json();
+}
+
+async function listarEscuderias(){
+    const resposta = await fetch('${API_URL}/escuderias');
+    return resposta.json();
+}
